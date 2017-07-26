1867ed0ef294224f3ef82cda07e87db6c36b422a
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-25 17:36:15 -0700

    Added method to create new students and added multiple jinja templates.

---
+++ hackbright_web.py
@@ -0,0 +1,53 @@
+"""A web application for tracking projects, students, and student grades."""
+
+from flask import Flask, request, render_template
+
+import hackbright
+
+app = Flask(__name__)
+
+
+@app.route("/student")
+def get_student():
+    """Show information about a student."""
+
+    github = request.args.get('github')
+
+    first, last, github = hackbright.get_student_by_github(github)
+
+    html = render_template("student_info.html",
+                           first=first,
+                           last=last,
+                           github=github)
+    return html
+
+
+@app.route("/student-search")
+def get_student_form(): 
+    """Show form for searching for a student """
+
+    return render_template("student_search.html")
+
+@app.route("/student-create")
+def show_new_student_form(): 
+    """Show form for creating a new student"""
+
+    return render_template("student_create.html")
+
+@app.route("/student-add", methods=['POST'])
+def student_add(): 
+    """Add a student. """
+
+    firstname = request.form.get("first_name")
+    lastname = request.form.get("last_name")
+    github = request.form.get("github_name")
+
+    hackbright.make_new_student(firstname, lastname, github)
+
+    return render_template("student_success.html", firstname=firstname,
+                            lastname=lastname, github=github)
+
+
+if __name__ == "__main__":
+    hackbright.connect_to_db(app)
+    app.run(debug=True)
